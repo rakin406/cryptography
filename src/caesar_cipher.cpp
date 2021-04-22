2852cc73a77c3c8854a95efea3dd934b79584547
rakin406 <rakinhacker@gmail.com>
2021-04-22 17:49:29 +0600

    Rename variable and parameter

--- src/caesar_cipher.cpp
+++ src/caesar_cipher.cpp
@@ -4,53 +4,53 @@
 #include <string>
 
 // Encrypt text using caesar cipher
-std::string encrypt_caesar_cipher(std::string text, int shifts = 3) {
-    std::string result = "";
+std::string encrypt_caesar_cipher(std::string plaintext, int shifts = 3) {
+    std::string ciphertext = "";
 
-    for (int i = 0; i < text.size(); ++i) {
-        if (std::isalpha(text[i])) {
-            char mod_char = text[i] + (char)shifts;
-            if ((std::islower(text[i]) && mod_char <= 'z') ||
-                (std::isupper(text[i]) && mod_char <= 'Z')) {
-                result += mod_char;
+    for (int i = 0; i < plaintext.size(); ++i) {
+        if (std::isalpha(plaintext[i])) {
+            char mod_char = plaintext[i] + (char)shifts;
+            if ((std::islower(plaintext[i]) && mod_char <= 'z') ||
+                (std::isupper(plaintext[i]) && mod_char <= 'Z')) {
+                ciphertext += mod_char;
             } else {
-                if (std::isupper(text[i])) {
-                    result += (int)'A' + (mod_char - (int)'Z') - 1;
+                if (std::isupper(plaintext[i])) {
+                    ciphertext += (int)'A' + (mod_char - (int)'Z') - 1;
                 } else {
-                    result += (int)'a' + (mod_char - (int)'z') - 1;
+                    ciphertext += (int)'a' + (mod_char - (int)'z') - 1;
                 }
             }
         } else {
-            result += text[i];
+            ciphertext += plaintext[i];
         }
     }
 
-    return result;
+    return ciphertext;
 }
 
 // Decrypt text using caesar cipher
-std::string decrypt_caesar_cipher(std::string text, int shifts = 3) {
-    std::string result = "";
+std::string decrypt_caesar_cipher(std::string ciphertext, int shifts = 3) {
+    std::string plaintext = "";
 
-    for (int i = 0; i < text.size(); ++i) {
-        if (std::isalpha(text[i])) {
-            char mod_char = text[i] - (char)shifts;
-            if ((std::islower(text[i]) && mod_char >= 'a') ||
-                (std::isupper(text[i]) && mod_char >= 'A')) {
-                result += mod_char;
+    for (int i = 0; i < ciphertext.size(); ++i) {
+        if (std::isalpha(ciphertext[i])) {
+            char mod_char = ciphertext[i] - (char)shifts;
+            if ((std::islower(ciphertext[i]) && mod_char >= 'a') ||
+                (std::isupper(ciphertext[i]) && mod_char >= 'A')) {
+                plaintext += mod_char;
             } else {
-                if (std::isupper(text[i])) {
-                    result += (int)'Z' - ((int)'A' - mod_char) + 1;
+                if (std::isupper(ciphertext[i])) {
+                    plaintext += (int)'Z' - ((int)'A' - mod_char) + 1;
                 } else {
-                    result += (int)'z' - ((int)'a' - mod_char) + 1;
+                    plaintext += (int)'z' - ((int)'a' - mod_char) + 1;
                 }
             }
         } else {
-            result += text[i];
+            plaintext += ciphertext[i];
         }
     }
 
-    return result;
+    return plaintext;
 }
 
 int main() {
