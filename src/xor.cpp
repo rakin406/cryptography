7e6d1af0ec42b1dea99fd692160e87296aa4ec93
rakin406 <rakinhacker@gmail.com>
2021-04-22 20:23:17 +0600

    One time pad works

--- src/xor.cpp
+++ src/xor.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <random>
 #include <string>
+
+// Generate random integer in range
+int generate_random(int min, int max) {
+    std::random_device rd;        // obtain a random number from hardware
+    std::mt19937 generator(rd()); // seed the generator
+    std::uniform_int_distribution<> dist(min, max); // define the range
+    return dist(generator);
+}
+
+// Generate random key with given length
+std::string generate_key(int length) {
+    std::string key = "";
+    for (int i = 0; i < length; ++i) {
+        key += (char)generate_random(33, 126);
+    }
+    return key;
+}
 
 // Encrypt and decrypt text with XOR cipher technique
 std::string xor_cipher(std::string text, std::string key) {
@@ -11,7 +29,5 @@ std::string xor_cipher(std::string text, std::string key) {
 }
 
 int main() {
-    std::cout << xor_cipher("hackerman", "KEY") << "\n";
-    std::cout << xor_cipher("#$:  +&$7", "KEY") << "\n";
     return 0;
 }
