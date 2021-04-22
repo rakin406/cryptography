60387acf48a0ebd81f5177eb15953c13676ca275
rakin406 <rakinhacker@gmail.com>
2021-04-22 10:41:28 +0600

    Decryption now works fine

--- src/caesar_cipher.cpp
+++ src/caesar_cipher.cpp
@@ -15,9 +15,9 @@ std::string encrypt_caesar_cipher(std::string text, int shifts = 3) {
                 result += mod_char;
             } else {
                 if (std::isupper(text[i])) {
-                    result += (char)64 + (mod_char - (char)90);
+                    result += (int)'A' + (mod_char - (int)'Z') - 1;
                 } else {
-                    result += (char)96 + (mod_char - (char)122);
+                    result += (int)'a' + (mod_char - (int)'z') - 1;
                 }
             }
         } else {
@@ -30,18 +30,35 @@ std::string encrypt_caesar_cipher(std::string text, int shifts = 3) {
 
 // Decrypt text using caesar cipher
 std::string decrypt_caesar_cipher(std::string text, int shifts = 3) {
-    std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+    std::string result = "";
 
     for (int i = 0; i < text.size(); ++i) {
+        if (std::isalpha(text[i])) {
+            char mod_char = text[i] - (char)shifts;
+            if ((std::islower(text[i]) && mod_char >= 'a') ||
+                (std::isupper(text[i]) && mod_char >= 'A')) {
+                result += mod_char;
+            } else {
+                if (std::isupper(text[i])) {
+                    result += (int)'Z' - ((int)'A' - mod_char) - 1;
+                } else {
+                    result += (int)'z' - ((int)'a' - mod_char) - 1;
+                }
+            }
+        } else {
+            result += text[i];
+        }
     }
 
-    return text;
+    return result;
 }
 
 int main() {
-    std::cout << encrypt_caesar_cipher("zebra") << "\n";
+    // std::cout << encrypt_caesar_cipher("zebra") << "\n";
     // std::cout << encrypt_caesar_cipher("good JOB") << "\n";
     // std::cout << encrypt_caesar_cipher(" ") << "\n";
-    // std::cout << decrypt_caesar_cipher("jrrg mre") << "\n";
+    std::cout << decrypt_caesar_cipher("jrrg mre") << "\n";
+    std::cout << decrypt_caesar_cipher("JRRG MRE") << "\n";
+    std::cout << decrypt_caesar_cipher(" .") << "\n";
     return 0;
 }
