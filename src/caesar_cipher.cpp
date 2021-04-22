47a9e9559a14a56b43edd1b47ad3a11358c59e5d
rakin406 <rakinhacker@gmail.com>
2021-04-22 09:56:57 +0600

    Finally got how encryption works

--- src/caesar_cipher.cpp
+++ src/caesar_cipher.cpp
@@ -1,63 +1,42 @@
 #include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <string>
 
 // Encrypt text using caesar cipher
 std::string encrypt_caesar_cipher(std::string text, int shifts = 3) {
-    std::string letters = "abcdefghijklmnopqrstuvwxyz";
-    std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+    std::string result = "";
 
     for (int i = 0; i < text.size(); ++i) {
-        for (int j = 0; j < letters.size(); ++j) {
-            if (text[i] == letters[j]) {
-                // Check last three letters in english alphabet
-                if (letters[j] == 'x') {
-                    text[i] = 'a';
-                } else if (letters[j] == 'y') {
-                    text[i] = 'b';
-                } else if (letters[j] == 'z') {
-                    text[i] = 'c';
-                } else {
-                    // Replace letter with third next letter
-                    text[i] = letters[j + shifts];
-                }
-                break;
+        if (std::isalpha(text[i])) {
+            char mod_char = text[i] + (char)shifts;
+            if (mod_char > 64 && mod_char < 123) {
+                result += mod_char;
+            } else {
+                result += (char)64 + (mod_char - (char)122);
             }
+        } else {
+            result += text[i];
         }
     }
 
-    return text;
+    return result;
 }
 
 // Decrypt text using caesar cipher
 std::string decrypt_caesar_cipher(std::string text, int shifts = 3) {
-    std::string letters = "abcdefghijklmnopqrstuvwxyz";
     std::transform(text.begin(), text.end(), text.begin(), ::tolower);
 
     for (int i = 0; i < text.size(); ++i) {
-        for (int j = 0; j < letters.size(); ++j) {
-            if (text[i] == letters[j]) {
-                // Check first three letters in english alphabet
-                if (letters[j] == 'a') {
-                    text[i] = 'x';
-                } else if (letters[j] == 'b') {
-                    text[i] = 'y';
-                } else if (letters[j] == 'c') {
-                    text[i] = 'z';
-                } else {
-                    // Replace letter with third previous letter
-                    text[i] = letters[j - shifts];
-                }
-                break;
-            }
-        }
     }
 
     return text;
 }
 
 int main() {
-    // std::cout << encrypt_caesar_cipher("good job") << "\n";
-    std::cout << decrypt_caesar_cipher("jrrg mre") << "\n";
+    std::cout << encrypt_caesar_cipher("zebra") << "\n";
+    // std::cout << encrypt_caesar_cipher("good JOB") << "\n";
+    // std::cout << encrypt_caesar_cipher(" ") << "\n";
+    // std::cout << decrypt_caesar_cipher("jrrg mre") << "\n";
     return 0;
 }
